65e3c9fb23f088dfec3e3f730b554898f774e92c
chicoreus <mole@morris.net>
2023-06-10 12:28:50 -0400

    Removing checked stub for AMENDMENT_EVENT_FROM_EVENTDATE.

--- src/main/java/org/filteredpush/qc/date/DwCEventDQ_stubs.java
+++ src/main/java/org/filteredpush/qc/date/DwCEventDQ_stubs.java
@@ -12,38 +12,6 @@ public class DwCEventDQ_stubs {
 
 
     /**
-     * Propose amendment to values in any of dwc:year, dwc:month, dwc:day, dwc:startDayOfYear or dwc:endDayOfYear from a the content of dwc:eventDate.
-     *
-     * Provides: AMENDMENT_EVENT_FROM_EVENTDATE
-     * Version: 2023-03-29
-     *
-     * @param startDayOfYear the provided dwc:startDayOfYear to evaluate
-     * @param eventDate the provided dwc:eventDate to evaluate
-     * @param year the provided dwc:year to evaluate
-     * @param month the provided dwc:month to evaluate
-     * @param day the provided dwc:day to evaluate
-     * @param endDayOfYear the provided dwc:endDayOfYear to evaluate
-     * @return DQResponse the response of type AmendmentValue to return
-     */
-    @Amendment(label="AMENDMENT_EVENT_FROM_EVENTDATE", description="Propose amendment to values in any of dwc:year, dwc:month, dwc:day, dwc:startDayOfYear or dwc:endDayOfYear from a the content of dwc:eventDate.")
-    @Provides("710fe118-17e1-440f-b428-88ba3f547d6d")
-    @ProvidesVersion("https://rs.tdwg.org/bdq/terms/710fe118-17e1-440f-b428-88ba3f547d6d/2023-03-29")
-    public DQResponse<AmendmentValue> amendmentEventFromEventdate(@ActedUpon("dwc:startDayOfYear") String startDayOfYear, @ActedUpon("dwc:eventDate") String eventDate, @ActedUpon("dwc:year") String year, @ActedUpon("dwc:month") String month, @ActedUpon("dwc:day") String day, @ActedUpon("dwc:endDayOfYear") String endDayOfYear) {
-        DQResponse<AmendmentValue> result = new DQResponse<AmendmentValue>();
-
-        //TODO:  Implement specification
-        // INTERNAL_PREREQUISITES_NOT_MET if dwc:eventDate is EMPTY 
-        // or contains an invalid value according to ISO 8601-1 or 
-        // dwc:eventDate is not wholly within one year; FILLED_IN one 
-        // or more EMPTY terms dwc:year, dwc:month, dwc:day, dwc:startDayOfYear, 
-        // dwc:endDayOfYear if any were unambiguously interpreted from 
-        // values in dwc:eventDate, and dwc:eventDate is wholly within 
-        // one year; otherwise NOT_AMENDED 
-
-        return result;
-    }
-
-    /**
      * Propose amendment of the value of dwc:eventDate to a valid ISO date.
      *
      * Provides: AMENDMENT_EVENTDATE_STANDARDIZED
